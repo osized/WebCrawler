19c20edb9789d6cba5060d5572761bbadfd0e730
Alexey <edgycircle@yandex.ru>
2017-10-18 01:01:19 +0300

    final version

--- src/main/java/com/ayudin/Main.java
+++ src/main/java/com/ayudin/Main.java
@@ -1,22 +1,28 @@
 package com.ayudin;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
     public static void main(String[] args) {
         ConcurrentMap <String, Integer> map = new ConcurrentHashMap<>();
         ConcurrentHashMap<String, Integer> resultHolder = new ConcurrentHashMap<>();
-        ParsePage parsePage = new ParsePage("ru.wikipedia.org", new ConcurrentHashMap<>(), resultHolder, 4);
+        ParsePage parsePage = new ParsePage("https://shipilev.net/", new ConcurrentHashMap<>(), resultHolder, 10);
+        ForkJoinPool pool = new ForkJoinPool();
         try {
-             parsePage.fork();
+             pool.submit(parsePage);
         } catch (Exception e){
             e.printStackTrace();
         }
         parsePage.join();
-        resultHolder.forEach((k, v) -> System.out.println(k + ": " + v));
+        resultHolder.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                .limit(100)
+                .forEach(System.out::println);
     }
 
 }
--- src/main/java/com/ayudin/ParsePage.java
+++ src/main/java/com/ayudin/ParsePage.java
@@ -1,5 +1,13 @@
 package com.ayudin;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,12 +33,22 @@ public class ParsePage extends RecursiveAction {
     @Override
     protected void compute() {
         try {
-            if (depth <= 0) return;
+            if (depth == 0) return;
             List<ParsePage> childTasks = new ArrayList<>();
-            String body = getPageBody();
-            countWords(body);
-            for (String link : getUniqueLinks(body)) {
+            String text;
+            Document page;
+            try {
+                 page = Jsoup.connect(url).get();
+                 text = page.text();
+            } catch (NullPointerException | IOException ioe){
+                //System.err.println("cannot load text: " + url);
+                return;
+            }
+
+            countWords(text);
+            for (String link : getUniqueLinks(page)) {
                 ParsePage newTask = new ParsePage(link, visitedPages, this.wordsMap, depth - 1);
+                System.out.println("forked: " + link);
                 newTask.fork();
                 childTasks.add(newTask);
             }
@@ -43,10 +61,24 @@ public class ParsePage extends RecursiveAction {
 
     }
 
-    private List<String> getUniqueLinks(String body) {
-        //jsoup goes here
-        List<String> allLinks = Arrays.asList("http://www.ya.ru");
-        List<String> uniqueLinks = allLinks.stream()
+
+    private List<String> getUniqueLinks(Document doc) {
+        Elements links = doc.select("a[href]");
+        List<String> internalLinks = new ArrayList<>();
+        for (Element link : links) {
+            String absolute = link.attr("abs:href");
+            try {
+                URL newUrl = new URL(absolute);
+                URL curUrl = new URL(url);
+                if (newUrl.getHost().equals(curUrl.getHost())) {
+                    internalLinks.add(absolute);
+                }
+            } catch (MalformedURLException ex) {
+                System.err.println("Malformed URL: " + absolute);
+            }
+        }
+
+        List<String> uniqueLinks = internalLinks.stream()
                 .distinct()
                 .filter(link ->
                         visitedPages.putIfAbsent(link, true) == null  //putIfAbsent returns null if link was not in visitedPages
@@ -58,14 +90,12 @@ public class ParsePage extends RecursiveAction {
     private void countWords(String text) {
         String[] words = text.split(" ");
         for (String word : words) {
-            wordsMap.putIfAbsent(word, 0);
-            wordsMap.merge(word, 1, (oldCount, newCount) -> oldCount + 1);
+            String uppercase = word.toUpperCase();
+            if (uppercase.matches("[A-ZА-Я]+")) {
+                wordsMap.putIfAbsent(uppercase, 0);
+                wordsMap.merge(uppercase, 1, (oldCount, newCount) -> oldCount + 1);
+            }
         }
-    }
-
-    //todo
-    private String getPageBody() {
-        return "aaa bbb ccc aaaa";
     }
 }
 
